06c60ad84e5cef0aa397e665c23f75732b4a42b2
David Oh <doh@thoughtworks.com>
2011-11-22 17:09:08 +1100

    added integration test for consultant list view

--- spec/views/consultants/index.html.erb_spec.rb
+++ spec/views/consultants/index.html.erb_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe "consultants/index.html.erb" do
   it "should display the list of consultants" do
-    assign(:consultants, [stub_model(Consultant, :name=> "Chicago", :email=>"chicago@thoughtworks.com", :role=>"Dev", :grade=>"Consultant", :skill=>"Ruby")])
+    assign(:consultants, [stub_model(Consultant, :name=> "Chicago", :email=>"chicago@thoughtworks.com", :role=>"Dev", :grade=>"Consultant", :all_skills => "Ruby")])
+
     render
     rendered.should contain("Chicago")
     rendered.should contain("chicago@thoughtworks.com")
